26c277d459d0ec0e28dd7159a1a48e39970cfcd1
Sch <junhg0211@gmail.com>
2022-10-24 15:16:30 +0900

    함수, 변수 타입 추가

--- rsp.py
+++ rsp.py
@@ -1,11 +1,13 @@
+Hand = int
+
 ROCK = 0
 SCISSORS = 1
 PAPER = 2
 
 
-def is_a_winning_b(a, b):
+def is_a_winning_b(a: Hand, b: Hand) -> bool:
     return (a - b) % 3 == 2
 
 
-def is_a_equal_b(a, b):
+def is_a_equal_b(a: Hand, b: Hand) -> bool:
     return a == b
