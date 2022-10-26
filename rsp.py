e61667e860709982b1d5add26889e8ce42fbea0a
andrewjeongtheawesome <imprayer1@naver.com>
2022-10-27 03:40:01 +0900

    랜덤으로 가위, 바위, 보중에 고르는 함수

--- rsp.py
+++ rsp.py
@@ -1,3 +1,4 @@
+import random
 Hand = int
 
 ROCK = 0
@@ -18,3 +19,7 @@ KOREAN_TABLE = {ROCK: '바위', PAPER: '보', SCISSORS: '가위'}
 
 def stringify(hand: Hand) -> str:
     return KOREAN_TABLE[hand]
+
+def random_choice() -> int:
+    return random.randrange(0,3)
+
