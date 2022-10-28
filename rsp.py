b99fc06a5b627b948e8a6842a8d06fd3189e1bee
andrewjeongtheawesome <imprayer1@naver.com>
2022-10-29 01:13:24 +0900

    여러가지 언어 추가

--- rsp.py
+++ rsp.py
@@ -22,10 +22,21 @@ def is_a_equal_b(a: Hand, b: Hand) -> bool:
 
 
 KOREAN_TABLE = {ROCK: '바위', PAPER: '보', SCISSORS: '가위'}
+ENGLISH_TABLE = {ROCK: 'ROCK', PAPER: 'PAPER', SCISSORS: 'SCISSORS'}
+MJB_TABLE = {ROCK: '묵', PAPER: '빠', SCISSORS: '찌'}
 
 
-def stringify(hand: Hand) -> str:
+def stringify_korean(hand: Hand) -> str:
     return KOREAN_TABLE[hand]
+
+
+def stringify_mjb(hand: Hand) -> str:
+    return MJB_TABLE[hand]
+
+
+def stringify_english(hand: Hand) -> str:
+    return ENGLISH_TABLE[hand]
+
 
 def random_choice() -> int:
     return random.randrange(0,3)
