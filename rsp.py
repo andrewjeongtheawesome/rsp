2e77f2aaa0786e03937272df617bbf0ded8b766d
Sch <junhg0211@gmail.com>
2022-10-29 07:53:30 +0900

    언어 enum을 통한 문자열화 구현

--- rsp.py
+++ rsp.py
@@ -14,21 +14,21 @@ def is_a_equal_b(a: Hand, b: Hand) -> bool:
     return a == b
 
 
-KOREAN_TABLE = {ROCK: '바위', PAPER: '보', SCISSORS: '가위'}
-MGB_TABLE = {ROCK: '묵', PAPER: '빠', SCISSORS: '찌'}
-ENGLISH_TABLE = {ROCK: 'ROCK', PAPER: 'PAPER', SCISSORS: 'SCISSORS'}
+KOREAN = 'korean'
+MGB = 'mgb'
+ENGLISH = 'english'
+LANGUAGE_TABLE = {
+    KOREAN: ['바위', '가위', '보'],
+    MGB: ['묵', '찌', '빠'],
+    ENGLISH: ['ROCK', 'SCISSORS', 'PAPER']
+}
+
+Language = str
 
 
-def stringify_korean(hand: Hand) -> str:
-    return KOREAN_TABLE[hand]
+def stringify(hand: Hand, language: Language = KOREAN) -> str:
+    return LANGUAGE_TABLE[language][hand]
 
-
-def stringify_mgb(hand: Hand) -> str:
-    return MGB_TABLE[hand]
-
-
-def stringify_english(hand: Hand) -> str:
-    return ENGLISH_TABLE[hand]
 
 def random_choice() -> int:
     return random.randrange(0,3)
