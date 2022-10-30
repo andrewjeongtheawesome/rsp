447f7869577210c0f83aa80569f362d15677b463
andrewjeongtheawesome <imprayer1@naver.com>
2022-10-30 22:44:59 +0900

    확률을 임의로 정할 수 있는 함수 구현

--- rsp.py
+++ rsp.py
@@ -1,4 +1,5 @@
 import random
+import math
 Hand = int
 
 ROCK = 0
@@ -14,22 +15,32 @@ def is_a_equal_b(a: Hand, b: Hand) -> bool:
     return a == b
 
 
-KOREAN = 'korean'
-MGB = 'mgb'
-ENGLISH = 'english'
-LANGUAGE_TABLE = {
-    KOREAN: ['바위', '가위', '보'],
-    MGB: ['묵', '찌', '빠'],
-    ENGLISH: ['ROCK', 'SCISSORS', 'PAPER']
-}
-
-Language = str
+KOREAN_TABLE = {ROCK: '바위', PAPER: '보', SCISSORS: '가위'}
 
 
-def stringify(hand: Hand, language: Language = KOREAN) -> str:
-    return LANGUAGE_TABLE[language][hand]
+def stringify(hand: Hand) -> str:
+    return KOREAN_TABLE[hand]
 
 
 def random_choice() -> int:
     return random.randrange(0,3)
 
+
+def probability():
+    global com_pro
+    com_pro = 0
+    com_pro = int(input("컴퓨터의 승률을 입력하세요.(단위:%,범위:0~100)"))/100
+    print("입력 완료 되었습니다.")
+
+
+def random_choice_probability(a: Hand) -> int:
+    left=round(((1-com_pro)/2),1)
+    if a==0:
+        char_choice=random.choices(range(0,3),weights=[left,com_pro,left],k=1)
+        return char_choice[0]
+    elif a==1:
+        char_choice=random.choices(range(0,3),weights=[left,left,com_pro],k=1)
+        return char_choice[0]
+    elif a==2:
+        char_choice=random.choices(range(0,3),weights=[com_pro,left,left],k=1)
+        return char_choice[0]
