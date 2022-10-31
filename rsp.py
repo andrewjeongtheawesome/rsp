7c41f3e31e6e337840b96e2615db775248ec76cd
andrewjeongtheawesome <imprayer1@naver.com>
2022-10-31 10:21:20 +0900

    확률 임의로 정하는 함수(새 브랜치)

--- rsp.py
+++ rsp.py
@@ -23,3 +23,23 @@ def stringify(hand: Hand) -> str:
 
 def random_choice() -> Hand:
     return random.randrange(0,3)
+
+
+def probability():
+    global com_pro
+    com_pro = 0
+    com_pro = int(input("컴퓨터의 승률을 입력하세요.(단위:%,범위:0~100)"))/100
+    print("입력 완료 되었습니다.")
+
+
+def random_choice_probability(a: Hand) -> int:
+    left=round(((1-com_pro)/2),1)
+    if a==0:
+        char_choice=random.choices(range(0,3),weights=[left,com_pro,left],k=1)
+        return char_choice[0]
+    elif a==1:
+        char_choice=random.choices(range(0,3),weights=[left,left,com_pro],k=1)
+        return char_choice[0]
+    elif a==2:
+        char_choice=random.choices(range(0,3),weights=[com_pro,left,left],k=1)
+        return char_choice[0]
